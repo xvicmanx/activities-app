d0dc114968478c258293faf1c86d65f1ec533610
Dayri Garcia Brito <dayrimax91@gmail.com>
2020-12-03 10:42:49 -0400

    configurando react native push notification

--- mobile/index.js
+++ mobile/index.js
@@ -2,5 +2,35 @@ import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import App from './src';
 import { name as appName } from './app.json';
+import PushNotification from 'react-native-push-notification';
+
+PushNotification.configure({
+  onRegister: function (token) {
+    console.log('TOKEN:', token);
+  },
+
+  onNotification: function (notification) {
+    console.log('NOTIFICATION:', notification);
+  },
+
+  onAction: function (notification) {
+    console.log('ACTION:', notification.action);
+    console.log('NOTIFICATION:', notification);
+  },
+
+  onRegistrationError: function (err) {
+    console.error(err.message, err);
+  },
+
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+
+  popInitialNotification: true,
+
+  requestPermissions: true,
+});
 
 AppRegistry.registerComponent(appName, () => App);
