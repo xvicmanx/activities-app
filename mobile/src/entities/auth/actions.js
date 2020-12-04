c40a5bbeba15d3a801268b90cc9d26056dcd3f97
Dayri Garcia Brito <dayrimax91@gmail.com>
2020-12-04 11:29:05 -0400

    local storage integration (#77)

--- mobile/src/entities/auth/actions.js
+++ mobile/src/entities/auth/actions.js
@@ -1,14 +1,32 @@
 import authSlice from './authSlice';
 import AuthService from './authService';
 import { ERRORS } from '../../constants';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const checkUserInfo = () => async (dispatch) => {
   try {
-    const res = await AuthService.checkUserInfo();
+    const token = await AsyncStorage.getItem('userToken');
+
+    if (!token) {
+      dispatch(authSlice.actions.setCurrentUser(null));
+      return;
+    }
+
+    const res = await AuthService.checkUserInfo(token);
 
     if (res.code === 'NOT_AUTHORIZED') {
       dispatch(authSlice.actions.setCurrentUser(null));
       return;
+    }
+
+    if (res.success) {
+      dispatch(
+        authSlice.actions.setCurrentUser({
+          exp: res.exp,
+          token: res.token,
+          ...res.user,
+        })
+      );
     }
   } catch (error) {
     console.log(error);
@@ -27,8 +45,10 @@ export const loginUser = (email, password) => async (dispatch) => {
     }
 
     if (res.success) {
+      await AsyncStorage.setItem('userToken', res.token);
+
       dispatch(
-        authSlice.actions.loginUser({
+        authSlice.actions.setCurrentUser({
           exp: res.exp,
           token: res.token,
           ...res.user,
