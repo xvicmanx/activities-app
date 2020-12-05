c9cba108fe63766540c70b97bfb76defa2d10362
Victor Trejo <vic3jo@gmail.com>
2020-12-05 10:13:29 -0400

    Restructure layout

---
+++ web/src/AdminApp.js
@@ -0,0 +1,84 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { faUsers, faHome, faBuilding } from '@fortawesome/free-solid-svg-icons'
+
+import AppLayout from './core/AppLayout';
+import { LoadingIndicator } from './components';
+
+import {
+  readTokenFromCookie,
+  loadUserFromToken,
+} from './entities/users/redux/UsersActions';
+
+import HomePage from './core/pages/HomePage/HomePage';
+import ActivitiesPage from './entities/activities/pages/ActivitiesPage';
+import CommunitiesPage from './entities/communities/pages/CommunitiesPage';
+import CommunityDetailsPage from './entities/communities/pages/CommunityDetailsPage';
+import UsersPage from './entities/users/pages/UsersPage';
+
+
+const AdminApp = (props) => {
+  const { Users } = useSelector(state => state);
+  const dispatch = useDispatch();
+  const token = readTokenFromCookie();
+
+  const shouldLoadToken = !Users.data && !Users.loading && token;
+
+  useEffect(() => {
+    dispatch(loadUserFromToken({
+      token,
+      shouldLoadToken,
+    }));
+  }, [dispatch, token, shouldLoadToken]);
+
+  if (!Users.data && !Users.loading && !token) {
+    return <Redirect to="/" />;
+  }
+
+  if (!Users.data) {
+    return (
+      <LoadingIndicator />
+    );
+  }
+  
+  return (
+    <AppLayout
+      {...props}
+      user={Users.data}
+      navigationLinks={[
+        {
+          path: '/home',
+          label: 'Inicio',
+          icon: faHome,
+        },
+        {
+          path: '/activities',
+          label: 'Actividades',
+          icon: faUsers,
+        },
+        {
+          path: '/communities',
+          label: 'Comunidades',
+          icon: faBuilding,
+        },
+        {
+          path: '/users',
+          label: 'Usuarios',
+          icon: faUsers,
+        },
+      ]}
+    >
+      <Switch>
+        <Route path="/activities" component={ActivitiesPage} />
+        <Route path="/communities" component={CommunitiesPage} exact />
+        <Route path="/communities/:id" component={CommunityDetailsPage} />
+        <Route path="/users" component={UsersPage} />
+        <Route path="/home" component={HomePage} />
+      </Switch>
+    </AppLayout>
+  );
+};
+
+export default AdminApp;
+
--- web/src/components/UserBasicInfo/UserBasicInfo.js
+++ web/src/components/UserBasicInfo/UserBasicInfo.js
@@ -3,12 +3,12 @@ import { Heading, Image } from 'react-bulma-components';
 
 import './UserBasicInfo.css';
 
-const UserBasicInfo = ({ image, user }) => {
+const UserBasicInfo = ({ user }) => {
   if (!user) return null;
   return (
     <div className="UserBasicInfo">
       <Image
-        src={image}
+        src={user.profileURL}
         alt="Pic"
         className="UserBasicInfo__Picture"
       />
