41654d0585b2ae2904716b3d6d44a84afbddd51a
Victor Trejo <vic3jo@gmail.com>
2020-12-08 17:27:37 -0400

    Fix requester issue

--- web/src/core/requester.test.js
+++ web/src/core/requester.test.js
@@ -55,4 +55,26 @@ describe('requester', () => {
       );
     });
   });
+
+  it('handles unexpected errors properly', async () => {
+    global.fetch = jest.fn(() => Promise.reject('Error'));
+    const result = await requester({
+      path: '/test-path',
+      method: 'POST',
+      payload: { name: 'test' },
+    });
+
+    expect(result).toEqual({ success: false, message: 'Unexpected Error' });
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(global.fetch).toHaveBeenCalledWith(
+      'http://localhost:4500/test-path',
+      {
+        body: '{"name":"test"}',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+      }
+    );
+  });
 });
